460ecf0b1b7014ccfec60fe1af7816f6438790f0
asahelcode <hoyx0101@gmail.com>
2023-10-01 12:05:26 +0100

    Done

---
+++ server.js
@@ -0,0 +1,23 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import express from "express";
+import cors from "cors";
+import uploadRoute from "./routes/uploadRoute.js";
+import recordingsRoute from "./routes/recordingsRoute.js";
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("uploads"));
+
+app.use("/api", uploadRoute);
+app.use("/api", recordingsRoute);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running successfully");
+});
